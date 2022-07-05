8c1b4c225d8bda711df21a0e9f6796d8815969a3
lana_cell <maulanamahmudi68@gmail.com>
2022-07-05 22:40:46 +0700

    tittle

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Toko Kita</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+  <p>Menyediakan produk pakaian dan sarung </p>
+  </body>
+</html>
+<?PHP
+?>
