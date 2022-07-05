4dfb6ab961759373ec29d1f07551ea1753cb53fd
bangroy <bangroy8420@gmail.com>
2022-07-05 23:02:08 +0700

    isi

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>Toko Kita</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="css/style.css" rel="stylesheet">
-  </head>
-  <body>
+
+<head>
+  <title>Toko Kita</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
   <p>Menyediakan produk pakaian dan sarung </p>
-  </body>
+
+  <h1>isinya isinya</h1>
+</body>
+
 </html>
 <?PHP
 ?>
